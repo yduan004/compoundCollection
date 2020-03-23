d1614d5546e6becd9f09f7c9d26b9b44b5c9fe08
yduan004 <yduan004@ucr.edu>
2020-03-22 22:26:45 -0700

    feat: create sqlite db for 4 annotation and 2 structure

---
+++ R/compoundCollection-package.R
@@ -0,0 +1,17 @@
+#' Annotation and Structure Collection of Small Molecules
+#'
+#' @name compoundCollection-package
+#' @aliases compoundCollection-package compoundCollection
+#' @docType package
+#' @description
+#' This package contains annotation and structures of drugs/small
+#' molecules in DrugAge, DrugBank, CMAP02 and LINCS databases, it also supports
+#' adding custom compound collections.
+#'
+#' @author
+#' \itemize{
+#'   \item Yuzhu Duan (yduan004@ucr.edu)
+#'   \item Thomas Girke (thomas.girke@ucr.edu)
+#' }
+
+NULL
